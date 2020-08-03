41ef4b4395e25df2841cac48ae5c7ba052f84e4f
sbgprojects <shubham.gujarathi@decartem.com>
2020-08-03 23:21:17 +0530

    aaa

--- js/main.js
+++ js/main.js
@@ -57,3 +57,40 @@ $(document).ready(function () {
     // initialize aos
     AOS.init();
 })
+
+
+$(document).ready(function () {
+    $('.navbar-nav a[href*=#]').bind('click', function (e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 1000, function () {
+            location.hash = target; //attach the hash (#jumptarget) to the pageurl
+        });
+
+        return false;
+    });
+});
+
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('section').each(function (i) {
+        if ($(this).position().top <= scrollDistance + 50) {
+            $('.navbar-nav a.active').removeClass('active');
+            $('.navbar-nav a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
